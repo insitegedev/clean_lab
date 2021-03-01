83446abe444b6e044384fc5ec0900cebf001033d
makhatadze <vitomaxatadze@gmail.com>
2021-03-01 15:48:58 +0400

    Add fb button.

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -1,3 +1,30 @@
+<div class="fb-customerchat" page_id="6708927376315932">
+</div>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution="setup_tool"
+     page_id="376359892852239"
+     logged_in_greeting="სალამი! მადლობა, რომ დაინტერესდით ჩვენი კომპანიით. რით შეგვიძლია დაგეხმაროთ? :blush:"
+     logged_out_greeting="სალამი! მადლობა, რომ დაინტერესდით ჩვენი კომპანიით. რით შეგვიძლია დაგეხმაროთ? :blush:">
+</div>
 <!-- header -->
 <header class="header {{Request::route()->getName() === 'homePage' ? 'transparent' : ''}}">
 
