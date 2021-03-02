c06746d84cbea3aa941b032a9f8f1caacf12a112
makhatadze <vitomaxatadze@gmail.com>
2021-03-02 10:04:14 +0400

    Add translations to dictionary module.

--- resources/views/admin/modules/dictionary/edit.blade.php
+++ resources/views/admin/modules/dictionary/edit.blade.php
@@ -5,20 +5,20 @@
         <div class="col-lg-6">
             <div class="element-wrapper">
             <h6 class="element-header">
-                @lang('admin.languages.update')
+                @lang('admin.update')
             </h6>
         <form action="{{route('DictionaryUpdate', [$locale,$language->id])}}"method="POST" class="bg-white py-3 px-4 grid w-full grid-cols-2 gap-3">
             @csrf
             @method('PUT')
 
             <div class="col-span-1 w-full ">
-                <small>@lang('admin.languages.key')</small>
+                <small>@lang('admin.key')</small>
                 <div class="form-control">
                     {{$language->key}}
                 </div>
             </div>
             <div class="col-span-1 w-full">
-                <small>@lang('admin.languages.module')</small>
+                <small>@lang('admin.module')</small>
                 <input required type="text" value="{{$language->module}}" name="module" class="form-control" placeholder="@lang('admin.languages.module')">
                 @error('module')
                 <p>{{$message}}</p>
@@ -31,7 +31,7 @@
                 </div>
             @endforeach
             <div class="border-t m-0 py-3 col-span-3">
-                <button class="btn btn-primary" type="submit">@lang('admin.languages.update')</button>
+                <button class="btn btn-primary" type="submit">@lang('admin.update')</button>
             </div>
         </form>
     </div>
